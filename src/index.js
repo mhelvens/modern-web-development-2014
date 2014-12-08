391f64db6a53b14b5c0f6154e26e7dee9205e0b5
mhelvens <mhelvens@gmail.com>
2014-12-09 00:12:56 +0100

    Version ready to present.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import './style.scss';
+import './index.scss';
 
-import $ from 'jquery';
-import P from 'bluebird';
+import $ from 'expose?jQuery!jquery';
+import Bacon from 'bacon';
+import Firebase from 'firebase';
+
+
+var fbRoot = new Firebase('//web-development-2014.firebaseio.com');
+var fbDrawings = fbRoot.child('drawings');
+var fbDrawingsStream = Bacon.fromEventTarget(fbDrawings, 'child_added').map('.val');
 
 $('document').ready(() => {
 
-	$('<div id="box">Hello World!</div>').appendTo('body');
+	/* the canvas */
+	var canvas = $('canvas');
+
+	/* convenience function */
+	var toCoords = (e) => [e.clientX - canvas.offset().left, e.clientY - canvas.offset().top];
+
+	/* capture drawings from the mouse */
+	var mouseDowns = canvas.asEventStream('mousedown').doAction('.preventDefault');
+	var mouseMoves = $(document).asEventStream('mousemove').doAction('.preventDefault');
+	var mouseUps = $(document).asEventStream('mouseup').doAction('.preventDefault');
+	var mouseDrawings = mouseDowns.flatMap(() => mouseMoves.map(toCoords).slidingWindow(2, 2).takeUntil(mouseUps));
+
+	/* capture drawings from the touchscreen */
+	var touchDowns = canvas.asEventStream('touchstart').doAction('.preventDefault');
+	var touchMoves = $(document).asEventStream('touchmove').doAction('.preventDefault');
+	var touchUps = $(document).asEventStream('touchend').doAction('.preventDefault');
+	var touchDrawings = touchDowns.flatMap(() => touchMoves.map('.originalEvent.touches.0').map(toCoords).slidingWindow(2, 2).takeUntil(touchUps));
+
+	/* publish mouse drawings to FireBase */
+	mouseDrawings.merge(touchDrawings).onValue((segment) => {
+		fbDrawings.push({
+			color: 'black',
+			segments: [segment]
+		});
+	});
+
+	/* display FireBase drawings on the canvas */
+	var context = canvas[0].getContext('2d');
+	fbDrawingsStream.map('.segments').flatMapConcat(Bacon.fromArray).onValue(( [[x1, y1], [x2, y2]] ) => {
+		context.beginPath();
+		context.lineWidth = 2;
+		context.moveTo(x1, y1);
+		context.lineTo(x2, y2);
+		context.stroke();
+	});
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,47 @@
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		'index-1': './src/index-1.js',
+		'index-2': './src/index-2.js',
+		'index': './src/index.js'
+	},
 	output: {
 		path: './dist',
-		filename: 'index.js'
+		filename: '[name].js'
+	},
+	resolve: {
+		modulesDirectories: ['node_modules', 'bower_components']
 	},
 	module: {
 		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'traceur?runtime' },
-			{ test: /\.scss$/, exclude: /node_modules/, loader: "style!css!autoprefixer!sass" }
+			{ test: /\.js$/, exclude: /node_modules|bower_components/, loader: 'traceur?runtime' },
+			{ test: /\.scss$/, exclude: /node_modules|bower_components/, loader: "style!css!autoprefixer!sass" }
 		]
 	},
 	plugins: [
-		new (require('html-webpack-plugin'))({
-			title: "Modern Web Development 2014",
-			template: "./src/index.html"
+		new webpack.ResolverPlugin(
+				new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+		),
+		new HtmlWebpackPlugin({
+			title: "Modern Web Development 2014 (1)",
+			template: "./src/index-1.html",
+			filename: "index-1.html"
+		}),
+		new HtmlWebpackPlugin({
+			title: "Modern Web Development 2014 (2)",
+			template: "./src/index-2.html",
+			filename: "index-2.html"
+		}),
+		new HtmlWebpackPlugin({
+			title: "Modern Web Development 2014 (final)",
+			template: "./src/index.html",
+			filename: "index.html"
 		})
+		//new HtmlWebpackPlugin({
+		//	title: "Modern Web Development 2014 (4)",
+		//	template: "./src/index-4.html",
+		//	filename: "index-4.html"
+		//})
 	]
 };
